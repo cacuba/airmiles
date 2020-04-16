98f505fcf13963f45cac21c4480ee8d295bcb78a
Caio Batista <cacuba@gmail.com>
2020-04-16 02:04:01 -0400

    Updates

---
+++ precache-manifest.a92284508d54ecd5c8369f87e0e43a95.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "666fb9c6544ccdf8cd584dadffa2ea98",
+    "url": "/airmiles/index.html"
+  },
+  {
+    "revision": "794ced575df43720bfac",
+    "url": "/airmiles/static/css/main.20b6342b.chunk.css"
+  },
+  {
+    "revision": "473c70ede8f584f489ea",
+    "url": "/airmiles/static/js/2.63760f12.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/airmiles/static/js/2.63760f12.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "794ced575df43720bfac",
+    "url": "/airmiles/static/js/main.cd8171cd.chunk.js"
+  },
+  {
+    "revision": "8c283b06c3c55e4b3f05",
+    "url": "/airmiles/static/js/runtime-main.8ff8185b.js"
+  }
+]);
